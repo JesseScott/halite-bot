b15caacec15ab1a5c88f5ff92ee9efffaf1d4f75
Jesse Scott <jesse.scott@eventbase.com>
2017-03-14 08:17:19 -0700

    updated python bot to optimized version

--- python/MyBot.py
+++ python/MyBot.py
@@ -4,9 +4,20 @@ import random
 
 
 myID, game_map = hlt.get_init()
-hlt.send_init("MyPythonBot")
+hlt.send_init("PythonBot")
+
+def assign_move(square):
+    for direction, neighbor in enumerate(game_map.neighbors(square)):
+        if neighbor.owner != myID and neighbor.strength < square.strength:
+            return Move(square, direction)
+
+    if square.strength < 5 * square.production:
+        return Move(square, STILL)
+    else:
+        return Move(square, random.choice((NORTH, WEST)))
+
 
 while True:
     game_map.get_frame()
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
+    moves = [assign_move(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
